8ec87dac9d8129f5b989500392bfdaa36abb3852
kajal kumari <kajalkumari@Mac.lan>
2026-06-05 20:42:16 +0530

    Update backend URL

--- src/api/url.js
+++ src/api/url.js
@@ -1,10 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
 const BACKEND_URL = axios.create({
-    baseURL: "https://note-taking-backend-7lke.onrender.com/"
-})
+  baseURL: "https://note-taking-backend-7lke.onrender.com/api/v1/noteapp"
+});
 
-export default BACKEND_URL
+export default BACKEND_URL;
 
 //backend connection with frontend 
 //http://localhost:4001/api/v1/noteapp/
