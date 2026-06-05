e1dc79af0f34db2127b42f78a5d63f2fa0eda169
kajal kumari <kajalkumari@Mac.lan>
2026-06-05 19:18:47 +0530

    Initial commit

---
+++ src/api/url.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+const BACKEND_URL = axios.create({
+    baseURL: "http://localhost:4001/api/v1/noteapp/"
+})
+
+export default BACKEND_URL
+
+//backend connection with frontend 
